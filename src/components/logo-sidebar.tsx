40c0592465dfe3ba37ccb05a45909d4ea253d598
Wandy Chandra Wijaya <wdycdr@gmail.com>
2025-04-29 18:01:15 +0700

    ganti tema

--- src/components/logo-sidebar.tsx
+++ src/components/logo-sidebar.tsx
@@ -19,7 +19,7 @@ export function LogoSidebar() {
           className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
         >
           <Link href='/' className='flex items-center'>
-            <div className='bg-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg'>
+            <div className='text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg'>
               <Image src='/Logo.png' width='300' height='300' alt='IPB Bike Logo' />
             </div>
             <div className='flex flex-col gap-0.5 leading-none'>
